d8464836bba271ab9fb6ed691f0a14ebe74039eb
sebas1193 <juansebasitan05@gmail.com>
2025-04-01 00:28:13 -0500

    feat: :rocket: add main logic integrating WiFi, RFID, and Buzzer modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#include "Buzzer.h"
+#include "RFID_mod.h"
+#include "WiFiConnection.h"
+#include "Credentials.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define SS_PIN 21
+#define RST_PIN 22
+#define BUZZER_PIN 25
+
+WiFiConnection wifi(WIFI_SSID, WIFI_PASSWORD);
+
+Buzzer buzzer(BUZZER_PIN, true);  // Inicialmente encendido
+RFID_mod rfid(SS_PIN, RST_PIN, UID_ESPERADO);
+
+bool accesoPermitido = false;  // Bandera para cortar el buzzer al primer escaneo correcto
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    wifi.begin();
+    rfid.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    wifi.update();  // Actualiza el estado de la conexión WiFi
+    if (!accesoPermitido && rfid.tarjetaDetectada()) {
+        buzzer.setStatus(false);  // Apaga el buzzer al detectar la tarjeta correcta
+        accesoPermitido = true;   // Bloquea nuevas activaciones
+        Serial.println("Acceso permitido, buzzer apagado.");
+    }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    buzzer.playTone();  // Solo se ejecutará si aún está activo
 }
